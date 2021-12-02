bf078d54c597a4259714f796f27ac92f7123a9b8
davidramos9112010 <81586209+davidramos9112010@users.noreply.github.com>
2021-12-01 18:06:05 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+prediction_1="";
+prediction_2="";
+
+
 Webcam.set({
 width:350,
 height:350,
 image_format:'png',
-png_quality:90
+png_quality:99
 });
 camera=document.getElementById("camera");
 Webcam.attach('#camera');
@@ -15,8 +19,82 @@ function take_snapshot()
 }
 
 console.log("ml5 version:",ml5.version);
-classifier=ml5.imageClassifier('/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/iu_lSNzes/model.json',modelLoaded);
 function modelLoaded(){
 console.log("model loaded!")
 
+}
+function speak()
+{
+    var synth = window.speechSynthesis;
+     speak_data_1 = "The first prediction is " + prediction_1; 
+     speak_data_2 = "And the second prediction is " + prediction_2; 
+     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+      synth.speak(utterThis); 
+}
+
+function check()
+{
+img=document.getElementById('captured_image');
+classifier.classify(img , gotResult);
+
+}
+function gotResult(error , results)
+{
+
+if(error)
+{
+console.error(error);
+}
+else
+{
+console.log(results);
+document.getElementById("result_emotion_name").innerHTML=results[0].label;
+document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+prediction_1=results[0].label;
+prediction_2=results[1].label;
+speak();
+if(results[0].label=="nice")
+{
+    document.getElementById("update_emoji").innerHTML="&#128076;";
+}
+if(results[0].label=="rock")
+{
+    document.getElementById("update_emoji").innerHTML="&#129304;";
+}
+
+if(results[0].label=="hi")
+{
+    document.getElementById("update_emoji").innerHTML="&#128075;";
+}
+if(results[0].label=="vsign")
+{
+    document.getElementById("update_emoji").innerHTML="&#9996;";
+}
+if(results[0].label=="stop")
+{
+    document.getElementById("update_emoji").innerHTML="&#128400;";
+}
+if(results[1].label=="nice")
+{
+    document.getElementById("update_emoji").innerHTML="&#128076;";
+}
+if(results[1].label=="rock")
+{
+    document.getElementById("update_emoji").innerHTML="&#129304;";
+}
+
+if(results[1].label=="hi")
+{
+    document.getElementById("update_emoji").innerHTML="&#128075;";
+}
+if(results[1].label=="vsign")
+{
+    document.getElementById("update_emoji").innerHTML="&#9996;";
+}
+if(results[1].label=="stop")
+{
+    document.getElementById("update_emoji").innerHTML="&#128400;";
+}
+}
 }
